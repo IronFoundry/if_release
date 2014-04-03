506433c02f9ed1ced07ef11161241a0013affe42
Bob Brumfield <bob.brumfield@gmail.com>
2014-04-03 16:07:18 -0700

    Add nuget pack and new directory structure

--- build.ps1
+++ build.ps1
@@ -1,79 +1,124 @@
-
+param(
+    $BuildType = 'dev',
+    $NuGetPackageUrl = '',
+    $NuGetApikey = '',
+    $ReleaseVersion = '0.0.0'
+    )
 #Assumes
 #  git in path
 #
 
 #
-# Build Source and Update Submodules
+# Base directories
 #
-$ReleaseName='master'
 $IFSourceDirectory = Convert-Path $PWD
+$BuildRootDir = "$IFSourceDirectory\Build"
+$ToolsDir = "$IFSourceDirectory\tools"
+
+# Nuget properties
+$NuGetExe = "$BuildRootDir\nuget\nuget.exe"
+$NuGetNuSpec = "$BuildRootDir\Default.Deploy.nuspec"
+
+# Staging Properties
 $StagingRootDir = "$IFSourceDirectory\staging"
-$StagingDir = "$StagingRootDir\$ReleaseName"
+$StagingDir = "$StagingRootDir\$ReleaseVersion"
+$StagingIFDataRoot = "$StagingDir\IronFoundry"
+$StagingDeaPackageRoot = "$StagingDir\dea_ng"
+$StagingWardenPackageRoot = "$StagingDir\warden"
+
 $ReleaseDir = "$IFSourceDirectory\release"
-$ToolsDir = "$PWD\tools"
 $ZipCmd = "$ToolsDir\7zip\7za.exe"
-$LogFile = "$PWD\$ReleaseName-build.log"
-$if_warden_version='0.0.0'
+$LogFile = "$IFSourceDirectory\$ReleaseVersion-build.log"
 
-Write-Host "Updating submodules"
-git submodule update --init --recursive
-
-Write-Host "Building Warden"
-.\if_warden\build.bat 
-
-Write-Host "Building GO WinRunner"
-Set-Location $IFSourceDirectory\dea_ng\go\
-$env:GOPATH="$IFSourceDirectory\dea_ng\go"
-go build winrunner
-
-Set-Location $IFSourceDirectory
-
-"Staging the release"
-
-Remove-Item $StagingRootDir -force -recurse -erroraction silentlycontinue | Out-Null
-New-Item $StagingDir -itemtype directory -Force | Out-Null
-
-$dirs = @(
-    'buildpack_cache', 
-    'dea_ng\crashes', 
-    'dea_ng\db', 
-    'dea_ng\droplets', 
-    'dea_ng\instances', 
-    'dea_ng\staging', 
-    'dea_ng\tmp', 
-    'log',
-    'package_cache',
-    'run',
-    'warden\containers')
-
-ForEach ($dir in $dirs)
+function UpdateSubmodules
 {
-    New-Item $StagingDir\$dir -itemtype directory -force | Out-Null 
+    Write-Host "Updating submodules"
+    git submodule sync --recursive
+    git submodule update --init --recursive
 }
 
-Copy-Item -Recurse $IFSourceDirectory\dea_ng $StagingDir\dea_ng\app -Container -Force
-Copy-Item -Recurse $IFSourceDirectory\if_warden\output\$if_warden_version\binaries $StagingDir\warden\app -Container -Force
-Copy-Item -Recurse $IFSourceDirectory\tools $StagingDir\tools -Container -Force
-
-$additionalFiles = @( 
-    'ironfoundry-install.ps1', 
-    'start-if-services.ps1', 
-    'stop-if-services.ps1',
-	'install-prerequisites.ps1',
-	'configure-dea.rb',
-	'README.md')
-
-ForEach($file in $additionalFiles)
+function BuildWarden()
 {
-    Copy-Item $file $StagingRootDir -Container -Force
+    Write-Host "Building Warden"
+    .\if_warden\build.bat Default /verbosity:minimal /p:BuildVersion="$ReleaseVersion"
 }
 
-"Creating the release"
+function BuildDirectoryServer()
+{
+    Write-Host "Building GO WinRunner"
+    Set-Location $IFSourceDirectory\dea_ng\go\
+    $env:GOPATH="$IFSourceDirectory\dea_ng\go"
+    go build winrunner
+    Set-Location $IFSourceDirectory
+}
 
-Remove-Item $ReleaseDir -recurse -force -erroraction silentlycontinue | Out-Null
-New-Item $ReleaseDir -itemtype directory -force | Out-Null 
+function StageRelease()
+{
+    Write-Host "Staging the release"
 
-. $ZipCmd a -sfx "$ReleaseDir\ironfoundry-$ReleaseName.exe" -r -y $StagingRootDir\* | Out-Null
+    Remove-Item $StagingRootDir -force -recurse -erroraction silentlycontinue | Out-Null
+    New-Item $StagingDir -itemtype directory -Force | Out-Null
+
+    $dirs = @(
+        'buildpack_cache', 
+        'dea_ng\config',
+        'dea_ng\crashes',     
+        'dea_ng\db', 
+        'dea_ng\droplets',     
+        'dea_ng\instances', 
+        'dea_ng\staging', 
+        'dea_ng\tmp', 
+        'log',
+        'package_cache',
+        'run',
+        'warden\containers')
+
+    ForEach ($dir in $dirs)
+    {
+        New-Item $StagingIFDataRoot\$dir -itemtype directory -force | Out-Null 
+    }
+
+    Copy-Item -Recurse $IFSourceDirectory\dea_ng $StagingDeaPackageRoot -Container -Force
+    Copy-Item -Recurse $IFSourceDirectory\if_warden\output\$ReleaseVersion\binaries $StagingWardenPackageRoot -Container -Force
+    Copy-Item -Recurse $IFSourceDirectory\tools $StagingDir\tools -Container -Force
+
+    $additionalFiles = @( 
+        'ironfoundry-install.ps1', 
+        'start-if-services.ps1', 
+        'stop-if-services.ps1',
+    	'install-prerequisites.ps1',
+    	'configure-dea.rb',
+    	'README.md')
+
+    ForEach($file in $additionalFiles)
+    {
+        Copy-Item $file $StagingRootDir -Container -Force
+    }
+}
+
+function ZipRelease()
+{
+    Write-Host "Creating the release"
+
+    Remove-Item $ReleaseDir -recurse -force -erroraction silentlycontinue | Out-Null
+    New-Item $ReleaseDir -itemtype directory -force | Out-Null 
+
+    . $ZipCmd a -sfx "$ReleaseDir\ironfoundry-$ReleaseVersion.exe" -r -y $StagingRootDir\* | Out-Null
+}
+
+
+function Package()
+{
+    Write-Host "Creating nuspec packages"
+
+    . $NuGetExe pack "$NuGetNuSpec" -Version $ReleaseVersion -Prop "Id=ironfoundry.dea_ng" -BasePath "$StagingDeaPackageRoot" -NoPackageAnalysis -NoDefaultExcludes -OutputDirectory "$ReleaseDir"
+}
+
+UpdateSubmodules
+BuildWarden
+BuildDirectoryServer
+StageRelease
+ZipRelease
+Package
 
 Set-Location $IFSourceDirectory
